a86851b94fe59cab1201a46a07a6d9430745a6f0
Max <43565398+maxim-mityutko@users.noreply.github.com>
2025-04-28 22:25:54 +0200

    [feature] Kubernetes Dashboard (#874)

--- kubernetes/scripts/02-dashboad-auth.sh
+++ kubernetes/scripts/02-dashboad-auth.sh
@@ -3,5 +3,5 @@
 
 microk8s kubectl annotate ingress kubernetes-dashboard \
 "nginx.ingress.kubernetes.io/configuration-snippet"=\
-"proxy_set_header Authorization \"Bearer $(microk8s kubectl get secret microk8s-dashboard-token -n kube-system -o jsonpath='{.data.token}' | base64 -d)\";" \
--n kube-system --overwrite
+"proxy_set_header Authorization \"Bearer $(microk8s kubectl get secret kubernetes-dashboard-token -n kube-dashboard -o jsonpath='{.data.token}' | base64 -d)\";" \
+-n kube-dashboard --overwrite
