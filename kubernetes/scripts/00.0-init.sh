1c0a69265be39d83d2a2c02bc3fe288aa06d8e67
Max <43565398+maxim-mityutko@users.noreply.github.com>
2025-05-02 00:34:41 +0200

    [feature] CoreDNS (#945)

--- kubernetes/scripts/00.0-init.sh
+++ kubernetes/scripts/00.0-init.sh
@@ -1,5 +1,4 @@
 # Setup addons
-microk8s enable dns
 # When NVIDIA drivers can be preinstalled on the node `--gpu-operator-driver host`, more info https://microk8s.io/docs/addon-gpu
 # microk8s enable nvidia --gpu-operator-driver host
 # Full restart
